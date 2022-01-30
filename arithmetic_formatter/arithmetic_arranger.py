0920fbad3586669897d2208935a89acf2a4214f9
Camila Bodack <camila.bodack@gmail.com>
2022-01-30 15:11:06 -0300

    Adjust functions

--- arithmetic_formatter/arithmetic_arranger.py
+++ arithmetic_formatter/arithmetic_arranger.py
@@ -1,9 +1,12 @@
+
 problems_list = ["32 - 698", "1 - 3801", "45 + 43", "123 + 49", "988 + 40"]
 
 
 def arithmetic_arranger(problems_list, result):
     operator_list = split_list(problems_list)
-    return operator_list
+    vertical_align = separe_operators(operator_list)
+    for item in vertical_align:
+        print(item)
 
 
 def split_list(list):
@@ -12,5 +15,16 @@ def split_list(list):
         item = item.split(" ")
         splitted_list.append(item)
     return splitted_list
+
+def separe_operators(list):
+    vertical_arrange = []
+    for item in list:
+        major_item = max(item)
+        separe_result = "-" * len(major_item)
+        item.append(separe_result)
+        for subitem in item:
+            vertical_arrange.append(subitem)
+    return vertical_arrange
+
 
 print(arithmetic_arranger(problems_list, result=True))
