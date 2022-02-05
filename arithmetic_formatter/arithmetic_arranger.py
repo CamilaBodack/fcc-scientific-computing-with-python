0b23646a517c64b02daae59a5f7336595f28e1bc
Camila Bodack <camila.bodack@gmail.com>
2022-02-05 13:35:21 -0300

    Append items by index

--- arithmetic_formatter/arithmetic_arranger.py
+++ arithmetic_formatter/arithmetic_arranger.py
@@ -18,13 +18,18 @@ def split_list(list):
 
 def separe_operators(list):
     vertical_arrange = []
-    for item in list:
+    lines = []
+    for index, item in enumerate(list):
         major_item = max(item)
-        separe_result = "-" * len(major_item)
+        separe_result = "-" * (len(major_item) + 2)
         item.append(separe_result)
-        for subitem in item:
-            vertical_arrange.append(subitem)
-    return vertical_arrange
+        for subitem in range(len(item)):
+            if item[1] == "+" or item[1] == "-":
+                item[0] =  f"{item[0]:>{len(separe_result)}}"
+                item[1] =  f"{item[1]} {item[2]:>{len(separe_result)-2}}"
+                separator =  f"{separe_result}"
+        lines.append(f"{item[0]}\n{item[1]}\n{separator}")
+    return lines
 
 
 print(arithmetic_arranger(problems_list, result=True))
