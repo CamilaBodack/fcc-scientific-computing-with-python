ac1b8d1e5a47302cd16d794f95d900066bc866df
Camila Bodack <camila.bodack@gmail.com>
2022-03-01 11:24:11 -0300

    Refactor functions

--- time_calculator/time_calculator.py
+++ time_calculator/time_calculator.py
@@ -44,17 +44,9 @@ def check_next_day(
     extra_day = str(next_day).split(".")[1]
     extra_day = extra_day[0]
     if first_number == 1 and int(extra_day) == 0:
-        if day_of_week:
-            day_week = next_day_of_week(day_of_week, first_number)
-            return f"{day_week} (next day)"
-        else:
-            return "(next day)"
+        return pre_formatter_next_day(day_of_week, first_number)
     elif initial_period == "PM" and final_period == "AM" and next_day < 1:
-        if day_of_week:
-            day_week = next_day_of_week(day_of_week, extra_day)
-            return f"{day_week} (next day)"
-        else:
-            return "(next day)"
+        return pre_formatter_next_day(day_of_week, extra_day)
 
 
 def count_days(hours: int) -> int:
@@ -75,17 +67,16 @@ def names_week_day():
     }
 
 
-def next_day_of_week(day: str, total_days: int):
+def next_day_of_week(day: str, checker: int):
     days = names_week_day()
     iter_days = iter(days.items())
-    if total_days == 0 or total_days == 1:
+    if checker == 0 or checker == 1:
         for name in iter_days:
             if name[1].casefold() == day.casefold():
                 if name[0] == 6:
                     return "Sunday"
-                else:
-                    next_item = next(iter_days)
-                    return next_item[1]
+                next_item = next(iter_days)
+                return next_item[1]
 
 
 def all_days_of_week(day: str, total_days: int):
@@ -116,8 +107,7 @@ def hour_after(total_hour: int) -> int:
     hour = total_hour % 12
     if hour == 0:
         return 12
-    else:
-        return hour
+    return hour
 
 
 def get_changes_in_period(total_hour: int, initial_period: str) -> str:
@@ -127,10 +117,47 @@ def get_changes_in_period(total_hour: int, initial_period: str) -> str:
     if int(mid) >= 5:
         if initial_period == "AM":
             return "PM"
-        elif initial_period == "PM":
-            return "AM"
-    else:
-        return initial_period
+        return "AM"
+    return initial_period
+
+
+def pre_formatter_next_day(day_of_week: str, checker: int) -> str:
+    if day_of_week:
+        day_week = next_day_of_week(day_of_week, checker)
+        return f"{day_week} (next day)"
+    return "(next day)"
+
+
+def formatter_next_day(
+    hour: int,
+    minuts: str,
+    next_day: str,
+    next_day_period: str,
+    final_period: str,
+    day_of_week: str,
+) -> str:
+    if day_of_week:
+        return f"{hour}:{minuts} {next_day_period}, {next_day}"
+    return f"{hour}:{minuts} {final_period} {next_day}"
+
+
+def formatter_long_period_days(
+    hour: int, minuts: str, days: int, final_period: str, day_of_week: str
+) -> str:
+    if day_of_week:
+        day_week = all_days_of_week(day_of_week, days)
+        return f"{hour}:{minuts} {final_period}, {day_week} ({days} days later)"
+    return f"{hour}:{minuts} {final_period} ({days} days later)"
+
+
+def formatter_periods_without_days_count(
+    hour: int, minuts: str, final_period: str, day_of_week: str
+):
+    return f"{hour}:{minuts} {final_period}, {day_of_week}"
+
+
+def formatter_simple(hour: int, minuts: str, final_period: str) -> str:
+    return f"{hour}:{minuts} {final_period}"
 
 
 def final_time(
@@ -143,7 +170,7 @@ def final_time(
 ):
     hour = int(start_hour) + int(durarion_hour)
     hour = hour + get_extra_hour(start_minuts, duration_minuts)
-    days = count_days(hour)
+    more_than_one_day = count_days(hour)
     initial_period = get_period(start)
     final_period = get_changes_in_period(hour, initial_period)
     next_day = check_next_day(hour, initial_period, final_period, day_of_week)
@@ -151,20 +178,18 @@ def final_time(
     hour = hour_after(hour)
     minuts = get_mod_minuts(start_minuts, duration_minuts)
     if next_day:
-        if day_of_week:
-            return f"{hour}:{minuts} {next_day_period}, {next_day}"
-        else:
-            return f"{hour}:{minuts} {final_period} {next_day}"
-    if days:
-        if day_of_week:
-            day_week = all_days_of_week(day_of_week, days)
-            return f"{hour}:{minuts} {final_period}, {day_week} ({days} days later)"
-        else:
-            return f"{hour}:{minuts} {final_period} ({days} days later)"
+        return formatter_next_day(
+            hour, minuts, next_day, next_day_period, final_period, day_of_week
+        )
+    elif more_than_one_day:
+        return formatter_long_period_days(
+            hour, minuts, more_than_one_day, final_period, day_of_week
+        )
     elif day_of_week:
-        return f"{hour}:{minuts} {final_period}, {day_of_week}"
-    else:
-        return f"{hour}:{minuts} {final_period}"
+        return formatter_periods_without_days_count(
+            hour, minuts, final_period, day_of_week
+        )
+    return formatter_simple(hour, minuts, final_period)
 
 
 print(add_time("8:16 PM", "466:02", "tuesday"))
