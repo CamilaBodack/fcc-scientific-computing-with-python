4a240bb2dabb7a623acefd88481e075694b999cc
Camila Bodack <camila.bodack@gmail.com>
2022-03-06 15:15:14 -0300

    fix withdraw condition

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -3,40 +3,36 @@ class Category:
         self.budget = budget
         self.ledger = []
 
-    def deposit(self, deposit_amount: int, description: str) -> list:
-        new_desc = self.set_default_str(description)
-        new_deposit = {"amount": deposit_amount, "description": new_desc}
+    def deposit(self, amount: int, description="") -> list:
+        new_desc = description
+        new_deposit = {"amount": amount, "description": new_desc}
         return self.ledger.append(new_deposit)
 
-    def set_default_str(self, info: str) -> str:
-        if not info:
-            return ""
-        return info
-
-    def withdraw(self, withdraw_amount: int, description: str) -> bool:
-        new_desc = self.set_default_str(description)
+    def withdraw(self, amount: int, description="") -> bool:
+        new_desc = description
         amount_ledger = self.check_amount_in_ledger()
-        new_withdraw = {"amount": -withdraw_amount, "description": new_desc}
-        if amount_ledger > 0:
+        new_withdraw = {"amount": ((amount) * -1), "description": new_desc}
+        if amount < amount_ledger:
             self.ledger.append(new_withdraw)
             return True
         return False
 
     def check_amount_in_ledger(self):
         total_amount = 0
-        for key, value in self.ledger:
-            if key == "amount":
-                total_amount = total_amount + value
+        for item in self.ledger:
+            for key, value in item.items():
+                if key == "amount":
+                    total_amount = total_amount + value
         return total_amount
-
 
     def has_amount(self, total_amount: int, withdraw_amount: int):
         return (total_amount - withdraw_amount) > 0
 
 
-
-
-
-
 def create_spend_chart(categories):
     pass
+
+
+food = Category("Food")
+food.deposit(100, "deposit")
+print(food.withdraw(100.10))
