c82d2fdcdfa4836f1b906538b67c50a15d7bfa4c
Camila Bodack <camila.bodack@gmail.com>
2022-03-07 20:25:39 -0300

    start formating final output

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -2,17 +2,20 @@ class Category:
     def __init__(self, budget: str):
         self.budget = budget
         self.ledger = []
+        title_line = f"***********{self.budget}**********".ljust(30, "*")
+        print(title_line)
+        for item in self.ledger:
+            print(f"{item}\n")
 
     def deposit(self, amount: int, description="") -> list:
-        new_desc = description
-        new_deposit = {"amount": amount, "description": new_desc}
+        new_deposit = {"amount": amount, "description": description}
         return self.ledger.append(new_deposit)
 
     def withdraw(self, amount: int, description="") -> bool:
-        new_desc = description
+        funds = self.check_funds(amount)
         amount_ledger = self.check_amount_in_ledger()
-        new_withdraw = {"amount": ((amount) * -1), "description": new_desc}
-        if amount < amount_ledger:
+        new_withdraw = {"amount": ((amount) * -1), "description": description}
+        if funds and amount < amount_ledger:
             self.ledger.append(new_withdraw)
             return True
         return False
@@ -29,15 +32,23 @@ class Category:
         return self.check_amount_in_ledger()
 
     def transfer(self, amount: int, budget_instance):
+        funds = self.check_funds(amount)
         actual_balance = self.get_balance()
         if amount > actual_balance:
             return False
-        self.ledger.append(
-            {"amount": -amount, "description": f"Transfer to {budget_instance.budget}"}
-        )
-        budget_instance.ledger.append(
-            {"amount": amount, "description": f"Transfer from {self.budget}"}
-        )
+        if funds:
+            self.ledger.append(
+                {"amount": -amount, "description": f"Transfer to {budget_instance.budget}"}
+            )
+            budget_instance.ledger.append(
+                {"amount": amount, "description": f"Transfer from {self.budget}"}
+            )
+            return True
+
+    def check_funds(self, amount: int):
+        balance = self.get_balance()
+        if amount > balance:
+            return False
         return True
 
 
