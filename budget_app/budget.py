7b8bfba331749df00951f1c4e486680b43631f28
Camila Bodack <camila.bodack@gmail.com>
2022-03-11 20:57:31 -0300

    start __str__ method

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -2,23 +2,33 @@ class Category:
     def __init__(self, budget: str):
         self.budget = budget
         self.ledger = []
+
+
+    def __str__(self):
         len_str_budget = len(str(self.budget))
         len_side = (30 - len_str_budget) // 2
-        title_line = str(
-            "".ljust(len_side, "*") + f"{self.budget}" + "".rjust(len_side, "*")
-        ).center(30, "*") + "\n"
-        ledger_line = ""
+        title_line = str("".ljust(len_side, "*") + f"{self.budget}" + "".rjust(len_side, "*")
+    ).center(30, "*") + "\n"
+        ledger_line = self.output_description() + self.output_mount() + "\n"
+        print(ledger_line)
     
-    def output(self):
-        ledger_line = ""
+    def output_description(self):
+        ledger_desc = ""
         for item in self.ledger:
             for key, value in item.items():
                 if key == "description":
-                    print(key, value)
-                    ledger_line = str(str(value).ljust(23, " "))
-                    ledger_line = f"{ledger_line:<23}"
-                    print(ledger_line)
-        return ledger_line
+                    ledger_desc = str(str(value).ljust(23, " ")[:23])
+        return ledger_desc
+
+    def output_mount(self):
+        ledger_mount = ""
+        for item in self.ledger:
+            for key, value in item.items():
+                if key == "amount":
+                    ledger_mount = str(str(value).ljust(7, " ")[:7])
+        return ledger_mount
+
+
 
     def deposit(self, amount: int, description="") -> list:
         new_deposit = {"amount": amount, "description": description}
@@ -76,4 +86,4 @@ food = Category("Food")
 entertainment = Category("Entertainment")
 food.deposit(900, "deposit")
 food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-print(food.output())
+print(food.output_mount())
