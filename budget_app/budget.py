8f8d84b53708d5ac67fc77ec88962c8e4b959c06
Camila Bodack <camila.bodack@gmail.com>
2022-03-15 20:35:51 -0300

    start function create_spend_chart

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -76,11 +76,17 @@ class Category:
 
 
 def create_spend_chart(categories: list) -> str:
-    pass
-
+    for item in categories:
+        print(str(item), "======(c_c)====")
+    print("Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  ")
 
 food = Category("Food")
 entertainment = Category("Entertainment")
+business = Category("Business")
 food.deposit(900, "deposit")
-food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-print(food.__str__())
+entertainment.deposit(900, "deposit")
+business.deposit(900, "deposit")
+food.withdraw(105.55)
+entertainment.withdraw(33.40)
+business.withdraw(10.99)
+create_spend_chart([business, food, entertainment])
